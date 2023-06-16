d4748e5e12d2b235b9fde1855faf0c4c436ae6d2
4lejandro02 <alejandromar.t012@gmail.com>
2023-06-15 21:25:57 -0500

    creando la función insertar usuarios

--- Admin/modelos/administrador.php
+++ Admin/modelos/administrador.php
@@ -1,6 +1,29 @@
 <?php
-class Administrador 
+include_once('../../conexion.php');
+class Administrador extends Conexion
 {
-	
+	public function __construct()
+	{
+		$this->db = parent::__construct();
+	}
+	public function agregarad($Nombread,$Apellidoad,$Usuarioad,$Passwordad,$Perfilad,$Estadoad)
+	{
+		$statement = $this->db->prepare("INSERT INTO usuarios(Nombreusu,Apellidousu,Usuario,Passwordusu,Perfil,Estado)values(:Nombread,:Apellidoad,:Usuarioad,:Passwordad,:Perfilad,:Estadoad)");
+
+		$statement->bindParam(":Nombread",$Nombreusu);
+		$statement->bindParam(":Apellidoad",$Apellidousu);
+		$statement->bindParam(":Usuarioad",$Usuario);
+		$statement->bindParam(":Passwordad",$Passwordusu);
+		$statement->bindParam(":Perfilad",$Perfil);
+		$statement->bindParam(":Estadoad",$Estado);
+		if($statement->execute())
+		{
+			echo "usuario registrado";
+		}
+		else
+		{
+			echo "No se puede realizar el registro";
+		}
+	}
 }
 ?>
